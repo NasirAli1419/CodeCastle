a462e2fae542f67691a0bb6821257d4524a6389b
Nasir Ali <44375565+NasirAli1419@users.noreply.github.com>
2021-05-25 22:46:29 +0530

    Add files via upload

---
+++ LinkedList/MiddleOfALinkedList.cpp
@@ -0,0 +1,15 @@
+//link for problem ->  https://leetcode.com/problems/middle-of-the-linked-list/
+
+    ListNode* middleNode(ListNode* head) {
+        
+        ListNode *temp1 = head;
+        ListNode *temp2 = head;
+        
+        while(temp1!=NULL&&temp1->next!=NULL){
+            temp1=temp1->next->next;
+            temp2=temp2->next;
+        }
+        
+        return temp2;
+        
+    }
