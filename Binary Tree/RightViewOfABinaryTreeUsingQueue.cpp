bf19a5f50990b5b58d991ac0d68b8da89c9a6fdd
Nasir Ali <44375565+NasirAli1419@users.noreply.github.com>
2021-06-10 09:14:17 +0530

    Add files via upload

---
+++ Binary Tree/RightViewOfABinaryTreeUsingQueue.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+};
+
+
+Node* newNode(int data)
+{
+    Node *temp = new Node;
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+
+void printLeftView(Node* root)
+{
+    if(!root){
+        return ;
+    }
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+
+        int size = q.size();
+
+        for(int i=0;i<size;i++){
+            Node * temp = q.front();
+            if(i==size-1){
+                cout<<temp->data<<" ";
+            }
+            q.pop();
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+
+        }
+    }
+
+}
+
+int main()
+{
+
+    Node* root = newNode(10);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(7);
+    root->left->right = newNode(8);
+    root->right->right = newNode(15);
+    root->right->left = newNode(12);
+    root->right->right->left = newNode(14);
+
+    printLeftView(root);
+}
