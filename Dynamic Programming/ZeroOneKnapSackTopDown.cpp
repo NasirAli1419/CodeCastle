b3935694f0cb84e8b08eeb86ac692c688451ad51
Nasir Ali <44375565+NasirAli1419@users.noreply.github.com>
2021-07-02 22:51:00 +0530

    Add files via upload

---
+++ Dynamic Programming/ZeroOneKnapSackTopDown.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int value[]={60,100,120};
+    int weight[]={10,20,30};
+    int W=50;
+    int n=3;
+
+    int dp[n+1][W+1];
+
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<W+1;j++){
+            if(i==0||j==0){
+                dp[i][j]=0;
+            }
+        }
+    }
+
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<W+1;j++){
+            if(weight[i-1]<=j){
+                dp[i][j]=max(value[i-1] + dp[i-1][j-weight[i-1]],dp[i-1][j]);
+            }
+            else{
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+
+    cout<<dp[n][W];
+
+
+}
